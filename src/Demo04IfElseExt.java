3df24408ffb69a7c1127295ed78e66126e02cedd
suDouDou <windream100@163.com>
2021-05-06 20:55:05 +0800

    init

---
+++ src/Demo04IfElseExt.java
@@ -0,0 +1,14 @@
+public class Demo04IfElseExt{
+    public static void main(String[] args) {
+        int x = 10;
+        int y;
+        if (x >= 3) {
+            y = 2 * x + 1;
+        } else if (x > -1 && x < 3) {
+            y = 2 * x;
+        } else {
+            y = 2 * x - 1;
+        }
+        System.out.println("结果是" + y);
+    }
+}
